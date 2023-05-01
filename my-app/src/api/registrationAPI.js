6355fe32e3704769ae4737e9e01d2a5c33315672
HlebHarshkou2002 <erichkrause2012@tut.by>
2023-05-01 08:55:14 +0300

    added a registration API

--- my-app/src/api/registrationAPI.js
+++ my-app/src/api/registrationAPI.js
@@ -3,19 +3,27 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "http://localhost:8001/api/",
   headers: {
-    "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
   },
 });
 
 export const registrationAPI = {
   registrateUser(email, password) {
+    let userData = {
+      "email": email,
+      "name": "pofig",
+      "password": password
+    };
     return instance.post(`auth/registration`, {
-      body: {
-        "email": email,
-        "name": "pofig",
-        "password": password
-      }
+      method: 'POST',
+      params: { 'api-version': '3.0' },
+      headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': 'your-rapidapi-key',
+          'X-RapidAPI-Host': 'microsoft-translator-text.p.rapidapi.com',
+      },
+      data: userData,
+      body: userData,
     }).then((response) => {
       return response;
     });
